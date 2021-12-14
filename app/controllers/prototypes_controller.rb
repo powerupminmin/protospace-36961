d12981df06bfbdf9d0714d72f8ccfa47be625172
powerupminmin <imaihiromi.0101@gmail.com>
2021-12-14 19:32:22 +0900

    before actionの修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,6 +1,6 @@
 class PrototypesController < ApplicationController
 before_action :authenticate_user!, except: [:index, :show]
-before_action :move_to_index, except: [:new, :index, :show]
+before_action :move_to_index, except: [:new, :index,:create, :show, :destroy]
 
   def index
     @prototype = Prototype.all
